31547786a03613f96c2af219469cf07ab2c24e5f
pmppy13 <marodavid229@gmail.com>
2026-07-04 10:48:46 +0100

    Rename home URL from 'home' to 'index'

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.home_view, name='home'),  # This defines 'home' URL name
+    path('', views.home_view, name='index'),  # This defines 'home' URL name
     path('about/', views.about_view, name='about'),
     path('contact/', views.contact_view, name='contact'),
     path('login/', views.login_view, name='login'),
