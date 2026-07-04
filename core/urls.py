7d3a034e6798a595ec0fed6a31d849b0e654a1ef
pmppy13 <marodavid229@gmail.com>
2026-07-04 20:38:23 +0100

    Remove login and signup routes from URLs
    Removed login and signup paths from urlpatterns.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.home_view, name='index'),
     path('about/', views.about_view, name='about'),
@@ -9,6 +8,4 @@ urlpatterns = [
     path('platforms/', views.platforms_view, name='platforms'),
     path('markets/', views.markets_view, name='markets'),
     path('analysis/', views.analysis_view, name='analysis'),
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),
 ]
