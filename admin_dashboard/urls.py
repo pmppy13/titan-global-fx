3dc89db7f441ca5e697bd155f3ddf91a858303b0
pmppy13 <marodavid229@gmail.com>
2026-07-04 22:32:49 +0100

    Add wallet address routes to admin dashboard

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('users/<int:user_id>/', views.admin_user_detail, name='user_detail'),
     path('users/<int:user_id>/enable-withdraw/', views.admin_enable_withdraw, name='enable_withdraw'),
     path('transactions/', views.admin_transactions, name='transactions'),
+    path('transactions/<int:transaction_id>/', views.admin_transaction_detail, name='transaction_detail'),
     path('transactions/<int:transaction_id>/action/', views.admin_transaction_action, name='transaction_action'),
     path('deposit-options/', views.admin_deposit_options, name='deposit_options'),
     path('deposit-options/<int:option_id>/edit/', views.admin_edit_deposit_option, name='edit_deposit_option'),
@@ -18,4 +19,6 @@ urlpatterns = [
     path('withdraw-options/<int:option_id>/delete/', views.admin_delete_withdraw_option, name='delete_withdraw_option'),
     path('terms/', views.admin_terms, name='terms'),
     path('terms/<int:terms_id>/edit/', views.admin_edit_terms, name='edit_terms'),
+    path('wallet-addresses/', views.admin_wallet_addresses, name='wallet_addresses'),
+    path('wallet-addresses/<int:address_id>/edit/', views.admin_edit_wallet_address, name='edit_wallet_address'),
 ]
